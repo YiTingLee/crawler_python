e8dfe3260474e03c10a7a8e8fb467f24e691e156
YiTingLee <ranye0719@gmail.com>
2017-07-20 11:12:07 +0800

    get basic data

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+c = r.content
+
+soup = BeautifulSoup(c,"html.parser")
+all = soup.find_all("div",{"class":"propertyRow"})
+
+for item in all:
+    print(item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ",""))
+    print(item.find_all("span",{"class":"propAddressCollapse"})[0].text)
+    print(item.find_all("span",{"class":"propAddressCollapse"})[1].text)
+    try:
+        print(item.find("span",{"class":"infoBed"}).find("b").text)
+    except:
+        print(None)
+    try:
+        print(item.find("span",{"class":"infoSqFt"}).find("b").text)
+    except:
+        print(None)
+    try:
+        print(item.find("span",{"class":"infoValueFullBath"}).find("b").text)
+    except:
+        print(None)
+    try:
+        print(item.find("span",{"class":"infoValueHalfBath"}).find("b").text)
+    except:
+        print(None)
+    print(" ")
